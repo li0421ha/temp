a7a37bf969d811a3b159473b5078eebb2c6c9336
unknown <15146564609@163.com>
2024-06-07 10:37:46 +0800

    change upload

--- code.c
+++ code.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
 
 int main(void){
-	printf("test code\n");
+
+
+
+
+
+
+	printf(" code1\n");
+	printf(" code1\n");
+	printf(" code1\n");
+	printf(" code1\n");
+	printf(" code1\n");
+	printf(" code1\n");
+	printf(" code1\n");
+	
+	printf(" code1\n");
 	return 0;
 }
