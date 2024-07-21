80024c12f02e3d672f1cdce2c0d5b1addc305919
li0421ha <15146564609@163.com>
2024-07-21 14:33:43 +0800

    测试首次提交

--- code.c
+++ code.c
@@ -2,6 +2,12 @@
 
 int main(void){
 	
-	printf("test codeing\n");
+	printf("testing code1..\n");
+	printf("testing code2..\n");
+	printf("testing code3..\n");
+
+ 	printf("testing code4..\n");
+        printf("testing code45..\n");
+        printf("testing code33..\n");
 	return 0;
 }
